5be1a1e1610e04b8f6e2ff910fd7041a6ea158b7
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-30 16:10:32 +0100

    Create index.php

---
+++ pages/index.php
@@ -0,0 +1,10 @@
+<?php
+namespace FriendsOfRedaxo\AssetImport;
+
+use rex_addon;
+use rex_be_controller;
+use rex_view;
+
+$addon = rex_addon::get('asset_import');
+echo rex_view::title($addon->i18n('title'));
+rex_be_controller::includeCurrentPageSubPath();
