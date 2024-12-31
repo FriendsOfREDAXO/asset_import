a10e65311ea5391b598d2c4a916dce8d6b1cafda
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-31 16:06:56 +0100

    Update ProviderInterface.php

--- lib/Provider/ProviderInterface.php
+++ lib/Provider/ProviderInterface.php
@@ -3,12 +3,43 @@ namespace FriendsOfRedaxo\AssetImport\Provider;
 
 interface ProviderInterface
 {
+    /**
+     * Get unique provider identifier
+     */
     public function getName(): string;
+    
+    /**
+     * Get display name
+     */
     public function getTitle(): string;
+    
+    /**
+     * Get FontAwesome icon identifier
+     */
     public function getIcon(): string;
+    
+    /**
+     * Check if provider is configured
+     */
     public function isConfigured(): bool;
+    
+    /**
+     * Get configuration fields
+     */
     public function getConfigFields(): array;
+    
+    /**
+     * Search for assets
+     */
     public function search(string $query, int $page = 1, array $options = []): array;
+    
+    /**
+     * Import an asset
+     */
     public function import(string $url, string $filename): bool;
+    
+    /**
+     * Get default search options
+     */
     public function getDefaultOptions(): array;
 }
