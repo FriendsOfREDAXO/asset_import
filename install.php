9683b0fb2dfed2b5467d132a620c3210b80f55af
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-03 23:54:12 +0100

    Update install.php

--- install.php
+++ install.php
@@ -15,7 +15,10 @@ if (!$this->hasConfig()) {
 }
 
 // Erstelle Cache-Tabelle mit rex_sql_table für bessere Updatefähigkeit
-\rex_sql_table::get(\rex::getTable('asset_import_cache'))
+$table = \rex_sql_table::get(\rex::getTable('asset_import_cache'));
+
+// Definiere die Struktur der Tabelle
+$table
     // Basis-Spalten
     ->ensureColumn(new \rex_sql_column('id', 'int(10) unsigned', false, null, 'auto_increment'))
     ->ensureColumn(new \rex_sql_column('provider', 'varchar(191)'))
@@ -28,30 +31,8 @@ if (!$this->hasConfig()) {
     ->setPrimaryKey('id')
     
     // Index für schnellere Suche
-    ->ensureIndex(new \rex_sql_index('provider_cache', ['provider', 'cache_key']))
-    
-    // UTF8MB4 für volle Unicode-Unterstützung
-    ->setCharset('utf8mb4')
-    ->setCollation('utf8mb4_unicode_ci')
-    
-    // Tabelle erstellen/aktualisieren
-    ->ensure();
+    ->ensureIndex(new \rex_sql_index('provider_cache', ['provider', 'cache_key']));
 
-// Setze Berechtigungen für das AddOn
-if (\rex::getUser() && \rex::getUser()->isAdmin()) {
-    $this->setProperty('perm', 'asset_import[]');
-}
+// Erstelle oder aktualisiere die Tabelle
+$table->ensure();
 
-// Prüfe ob das med_copyright Feld existiert, aber versuche nicht es anzulegen
-$sql = \rex_sql::factory();
-$sql->setQuery('SHOW COLUMNS FROM ' . \rex::getTable('media') . ' LIKE "med_copyright"');
-
-// Wenn es nicht existiert, gib einen Hinweis aus
-if ($sql->getRows() === 0) {
-    \rex_logger::logError(
-        E_WARNING,
-        'The field "med_copyright" does not exist in the media table. Copyright information will not be saved.',
-        __FILE__,
-        __LINE__
-    );
-}
