f80d55a1d7329f173bdbb225754e04e1a25899b6
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-03 23:07:19 +0100

    Update ProviderInterface.php

--- lib/Provider/ProviderInterface.php
+++ lib/Provider/ProviderInterface.php
@@ -11,4 +11,16 @@ interface ProviderInterface
     public function search(string $query, int $page = 1, array $options = []): array;
     public function import(string $url, string $filename): bool;
     public function getDefaultOptions(): array;
+    
+    /**
+     * Get copyright information for imported media
+     * @return string|null Returns copyright information or null if not available
+     */
+    public function getCopyrightInfo(array $item): ?string;
+
+    /**
+     * Get field mapping configuration for provider
+     * @return array Returns array of mappable fields
+     */
+    public function getFieldMapping(): array;
 }
